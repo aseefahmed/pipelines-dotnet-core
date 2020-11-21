d16d1587c273ec220507446cb1eab8eddd533f3e
Aseef Ahmed <aseefahmed@gmail.com>
2020-11-21 13:53:48 +1300

    AB#163 new concept

--- Startup.cs
+++ Startup.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+//434343
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
